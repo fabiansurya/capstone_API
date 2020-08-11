6a60236b9e8e0c367738a7098407dbde61935e3a
fabiansurya <fabian.surya@ymail.com>
2020-08-11 14:20:11 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -15,12 +15,13 @@ def genre():
     conn = sqlite3.connect("data/chinook.db")
     invoice2012 = pd.read_sql_query(
     '''
-    SELECT genres.Name as Genre, invoices.*, invoice_items.InvoiceLineId
+    SELECT genres.Name as Genre, invoice_items.InvoiceLineId, invoices.BillingCity,
+    invoices.BillingCountry, invoices.CustomerId, invoices.InvoiceDate, invoices.Total  
     FROM invoices
     LEFT JOIN invoice_items ON invoices.InvoiceId = invoice_items.InvoiceId
     LEFT JOIN tracks ON tracks.TrackId = tracks.TrackId
     LEFT JOIN genres ON genres.GenreId = tracks.GenreId
-    WHERE Genre = 'Rock' AND invoices.InvoiceDate >= '2012-01-01'
+    WHERE Genre = 'Rock' AND invoices.InvoiceDate >= '2012-06-01'
     ''',
     conn
     )
@@ -31,7 +32,7 @@ def min_rating(value):
     book = pd.read_csv('data/books_c.csv')
     mask = book['average_rating'] <= float(value)
     book = book[mask]
-    return (book.to_json()) 
+    return (book.to_json())  
 
 @app.route("/docs")
 def documentation():
